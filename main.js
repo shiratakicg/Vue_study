53c6602c0315ade8171f56bc2bc1756d658027bc
shiratakicg <yuki-shira4423@i.softbank.jp>
2021-08-17 21:33:30 +0900

    CHAPTER4 Section16 Start

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 new Vue({
     el:'#app',
     data: {
-        scrollY: 0,
-        timer: null
+        width: 800,
+        height: 600
     },
-    created: function() {
-        //ハンドラを登録
-        window.addEventListener('scroll', this.handleScroll)
-    },
-    beforeDestroy: function() {
-        //ハンドラを解除（コンポーネントやSPAの場合忘れずに！）
-        window.removeEventListener('scroll', this.handleScroll)
-    },
-    methods: {
-        //違和感のない程度に200ms間隔でscrollデータを更新する例
-        handleScroll: function() {
-            if(this.timer === null){
-                this.timer = setTimeout(function(){
-                    this.scrollY = window.scrollY
-                    clearTimeout(this.timer)
-                    this.timer = null
-                }.bind(this), 200)
+    computed: {
+        //算出プロパティhalfWidthを定義
+        halfWidth: function() {
+            return this.width / 2
+        },
+        halfHeight: function() {
+            return this.height /2
+        },
+        //[width * height]の中心座標をオブジェクトで返す
+        halfPoint: function() {
+            return {
+                x: this.halfWidth,
+                y: this.halfHeight
             }
         }
     }
