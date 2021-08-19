0ec81d031a81e7eca535df589e7dd04f81994cb2
shiratakicg <yuki-shira4423@i.softbank.jp>
2021-08-19 23:28:31 +0900

    CHAPTER4 Section16 Finished

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 new Vue({
-    el:'#app',
+    el: '#app',
     data: {
-        width: 800,
-        height: 600
+        order: false,
+        //フォームの入力と紐付けるデータ
+        budget: 300,
+        //表示件数
+        limit: 2,
+        list: [
+            { id: 1, name: 'りんご', price: 100},
+            { id: 2, name: 'ばなな', price: 200},
+            { id: 3, name: 'いちご', price: 400},
+            { id: 4, name: 'おれんじ', price: 300},
+            { id: 5, name: 'めろん', price: 500}
+        ]
     },
     computed: {
-        //算出プロパティhalfWidthを定義
-        halfWidth: function() {
-            return this.width / 2
-        },
-        halfHeight: function() {
-            return this.height /2
-        },
-        //[width * height]の中心座標をオブジェクトで返す
-        halfPoint: function() {
-            return {
-                x: this.halfWidth,
-                y: this.halfHeight
-            }
-        }
+      matched: function() {
+          //budget以下のリストを返す算出プロパティ
+          return this.list.filter(function(el) {
+              return el.price <= this.budget
+          }, this)
+      },
+      // sortedを新しく追加
+      sorted: function() {
+        return _.orderBy(this.matched, 'price', this.order ? 'desc' : 'asc')
+      },
+      //matchedで返ったデータをlimit件返す算出プロパティ
+      limited: function() {
+          return this.sorted.slice(0, this.limit)
+      }
     }
-})
+  })
