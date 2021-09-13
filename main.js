44660258d9626b842f4c1bc42c5a8546474d41aa
shiratakicg <yuki-shira4423@i.softbank.jp>
2021-09-13 22:47:35 +0900

    CHAPTER5 Section25

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-new Vue({
-  el: '#app',
-  directives: {
-    focus: {
-      // 紐付いている要素がDOMに挿入されるとき
-      inserted: function (el) {
-        el.focus() // 要素にフォーカスを当てる
+Vue.component('my-component', {
+  template: '<div class="my-component">\
+  <p>名前.{{ name }} HP.{{ hp }}</p>\
+  <p>名前 <input v-model="localName"></p>\
+  <p>HP <input size="5" v-model.number="localHp"></p>\
+  </div>',
+  props: {
+    name: String,
+    hp: Number
+  },
+  computed: {
+    // 算出プロパティのセッター＆ゲッターを使ってv-modelを使用
+    localName: {
+      get: function () {
+        return this.name
+      },
+      set: function (val) {
+        this.$emit('update:name', val)
+      }
+    },
+    localHp: {
+      get: function () {
+        return this.hp
+      },
+      set: function (val) {
+        this.$emit('update:hp', val)
       }
     }
+  }
+})
+new Vue({
+  el: '#app',
+  data: {
+    name: 'スライム',
+    hp: 100
   }
 })
